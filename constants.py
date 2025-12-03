5559973b97e8a2c79edca7a9e5e089b7d435fafd
JanPetr287 <jan.petr287@gmail.com>
2025-12-03 18:22:42 +0100

    Added start of 3D wing analysis code

---
+++ constants.py
@@ -0,0 +1,12 @@
+const = {
+    # General
+    'velocity': 19.2, # m/s
+    'chord': 0.16, # m
+    'sea_level_density': 1.225, # kg/m^3
+
+
+    # 3D properties
+    'surface_area_3D': 0.066146, # m^2
+    'span_3D': 0.4169*2 # m
+
+}
