a7f69c1721125e83dc56b6984356265e5fd36741
JanPetr287 <jan.petr287@gmail.com>
2025-12-12 23:12:14 +0100

    Made a second analysis for 2d wing, same results :(

--- 3d&graphing.py
+++ 3d&graphing.py
@@ -3,9 +3,10 @@ import math as m
 import matplotlib.pyplot as plt
 
 from constants import const
-from experimental_2d import test_cases
+#from experimental_2d import test_cases
 from xflr_2d import plot_cp_at_alpha, xflr_alpha_2d, xflr_Cd_2d, xflr_Cl_2d
 
+from try2_experimental import Cl, alpha
 
 # ----- Loading Data -----
 exp_data_3d = np.genfromtxt('raw_Group8_3d.txt', skip_header=2)
@@ -22,8 +23,8 @@ xflr_Cl = xflr_data_3d[:,2] # [-]
 xflr_Cd = xflr_data_3d[:,5] # [-]
 xflr_Cdi = xflr_data_3d[:, 3] # [-]
 
-exp_2D_alpha = np.array([test_cases[i].alpha for i in test_cases])
-exp_2D_Cl = np.array([test_cases[i].c_lift for i in test_cases])
+# exp_2D_alpha = np.array([test_cases[i].alpha for i in test_cases])
+# exp_2D_Cl = np.array([test_cases[i].c_lift for i in test_cases])
 
 # ----- Calculations -----
 #exp_q = 1/2 * exp_rho * const['velocity']**2 
@@ -33,11 +34,18 @@ exp_Cd = exp_drag_F / (exp_q * const['surface_area_3D'])
 
 A = const['span_3D']/const['chord']
 print(A)
-print(A)
+
+# # Finding slope of 2d
+# key = (exp_2D_alpha > 0) & (exp_2D_alpha < 10)
+# a2D, intercept_2d = np.polyfit(exp_2D_alpha[key], exp_2D_Cl[key], 1)  # [1/deg], [-]
+# a2D = a2D*180/m.pi
+# #a2D += 0.5
+# print(a2D)
 
 # Finding slope of 2d
-key = (exp_2D_alpha > 0) & (exp_2D_alpha < 10)
-a2D, intercept_2d = np.polyfit(exp_2D_alpha[key], exp_2D_Cl[key], 1)  # [1/deg], [-]
+alpha, Cl = np.rad2deg(np.array(alpha)), np.array(Cl)
+key = (alpha > 0) & (alpha < 10)
+a2D, intercept_2d = np.polyfit(alpha[key], Cl[key], 1)  # [1/deg], [-]
 a2D = a2D*180/m.pi
 #a2D += 0.5
 print(a2D)
@@ -66,7 +74,8 @@ linear_cl_3D = linear_alpha_3D * a3D * m.pi/180 + intercept_3d
 
 # ----- PLotting -----
 # Plotting 2D vs 3D data
-plt.plot(exp_2D_alpha, exp_2D_Cl, marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
+# plt.plot(exp_2D_alpha, exp_2D_Cl, marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
+plt.plot(alpha, Cl, marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
 plt.plot(exp_alpha, exp_Cl, marker='^', markersize=5,markerfacecolor='lightblue', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
 plt.grid()
 plt.xlabel(r'$\alpha$ [deg]')
@@ -94,7 +103,8 @@ plt.legend(('Experiment', 'XFLR5'))
 plt.show()
 
 # Plotting Clalpha best fit line
-plt.plot(exp_2D_alpha, exp_2D_Cl, marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
+# plt.plot(exp_2D_alpha, exp_2D_Cl, marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
+plt.plot(alpha, Cl, marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
 plt.plot(linear_alpha_2D, linear_cl_2D, linestyle='dotted', linewidth=1, color='black')
 plt.grid()
 plt.xlabel(r'$\alpha$ [deg]')
@@ -130,7 +140,8 @@ plt.legend(('Experiment', 'XFLR5'))
 plt.show()
 
 # Plotting 2d xflr vs experimental
-plt.plot(exp_2D_alpha[:29], exp_2D_Cl[:29], marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
+# plt.plot(exp_2D_alpha[:29], exp_2D_Cl[:29], marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
+plt.plot(alpha[:29], Cl[:29], marker='o', markersize=5,markerfacecolor='orange', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
 plt.plot(xflr_alpha_2d[:-1], xflr_Cl_2d[:-1], marker='^', markersize=5,markerfacecolor='lightblue', markeredgecolor='black', linestyle='-', linewidth=1.5, color='black')
 plt.grid()
 plt.xlabel(r'$\alpha$ [deg]')
@@ -138,20 +149,20 @@ plt.ylabel(r'C$_{\text{l}}$ [-]')
 plt.legend(('Experiment', 'XFOIL'))
 plt.show()
 
-# Plotting pressure coefficient distribution at a given AOA
-AOA = float(input("AOA for pressure coefficients comparison [deg]: "))
-plot_cp_at_alpha(AOA)
-x_axis = np.linspace(0, 1, 100)
-if AOA <= 8:
-    i = (AOA + 5) * 2 + 1
-else:
-    i = (AOA - 8) * 2 + 14
-plt.plot(x_axis, test_cases[i].cp_normal_pressureSide_distribution(x_axis), label="Experiment", color='black')
-plt.plot(x_axis, test_cases[i].cp_normal_suctionSide_distribution(x_axis), color='black')
-plt.xlabel("Position along chord [-]")
-plt.ylabel(r"Pressure coefficient c$_{\text{p}}$ [-]")
-plt.legend()
-plt.gca().invert_yaxis()
-plt.grid(True, axis="both")
-plt.axhline(y=0, color='k', linewidth=0.5)
-plt.show()
+# # Plotting pressure coefficient distribution at a given AOA
+# AOA = float(input("AOA for pressure coefficients comparison [deg]: "))
+# plot_cp_at_alpha(AOA)
+# x_axis = np.linspace(0, 1, 100)
+# if AOA <= 8:
+#     i = (AOA + 5) * 2 + 1
+# else:
+#     i = (AOA - 8) * 2 + 14
+# plt.plot(x_axis, test_cases[i].cp_normal_pressureSide_distribution(x_axis), label="Experiment", color='black')
+# plt.plot(x_axis, test_cases[i].cp_normal_suctionSide_distribution(x_axis), color='black')
+# plt.xlabel("Position along chord [-]")
+# plt.ylabel(r"Pressure coefficient c$_{\text{p}}$ [-]")
+# plt.legend()
+# plt.gca().invert_yaxis()
+# plt.grid(True, axis="both")
+# plt.axhline(y=0, color='k', linewidth=0.5)
+# plt.show()
